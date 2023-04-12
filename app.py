bfdbcdf9569933751e2f8baba7fc7b3cf6d31886
I Durga Sai Manvitha <79002681+ROCKEY-DOG@users.noreply.github.com>
2023-04-12 14:49:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "manvitha" # write your name
+    age = "15" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/father")
+
+def father():
+    name = "Amar Kumar" 
+    age = "40" 
+
+    return render_template('father.html' , name = name , age = age)
+
+
+# define the route to mother webpage
+@app.route("/mother")
+ 
+def mother():
+    name = "Madhavi Latha"
+    age = "36"
+
+    return render_template('mother.html', name = name, age=age)
+
+
+# define the route to friends webpage
+@app.route("/friend")
+
+def friend():
+    name = "Ojaswini"
+    age = "15"
+
+    return render_template('friend.html', name = name, age= age)
+
+
+# add other routes, if you want
+@app.route("/sister")
+
+def sister():
+    name = "Deekshu"
+    age = "12"
+
+    return render_template('sister.html', name = name, age= age)
+ 
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
